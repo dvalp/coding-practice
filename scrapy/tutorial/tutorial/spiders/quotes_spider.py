b4e3f017909b5eca67f9ef4f90f3908c3465e279
David Valpey <greengargoyle@hotmail.com>
2017-02-07 03:11:28 -0800

    crawling multiple pages in the same site

--- scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -16,3 +16,8 @@ class QuotesSpider(scrapy.Spider):
                     'tags': quote.css('div.tags a.tag::text').extract()
                     }
 
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
