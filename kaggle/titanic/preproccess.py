d65c8fcf5cb4db855967686e3c15cdd33153d15c
David Valpey <greengargoyle@hotmail.com>
2017-04-08 00:28:12 -0700

    use expand to capture deck and cabin number

--- kaggle/titanic/preproccess.py
+++ kaggle/titanic/preproccess.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 df = pd.read_csv("data/train.csv")
-df['Deck'] = df[~df['Cabin'].isnull()]['Cabin'].str[0]
+# df['Deck'] = df[~df['Cabin'].isnull()]['Cabin'].str[0]
+df[['Deck', 'CabinNumber']] = df['Cabin'].str.extract('(\w)(\d+)')
 df['Title'] = df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
 df['FamSize'] = df['Parch'] + df['SibSp']
 
