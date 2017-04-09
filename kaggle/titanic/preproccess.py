7c5c20ac7fc2ccdaccc65b588e45e92b45ee7202
David Valpey <greengargoyle@hotmail.com>
2017-04-09 00:49:59 -0700

    Dummy variables for deck number and title

--- kaggle/titanic/preproccess.py
+++ kaggle/titanic/preproccess.py
@@ -7,5 +7,6 @@ df = pd.read_csv("data/train.csv")
 df[['Deck', 'CabinNumber']] = df['Cabin'].str.extract('(\w)(\d+)')
 df['Title'] = df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
 df['FamSize'] = df['Parch'] + df['SibSp']
-
+df[['DeckA', 'DeckB', 'DeckC', 'DeckD', 'DeckE', 'DeckF', 'DeckG']] = df['Deck'].str.get_dummies()
+df = pd.concat([df, df['Title'].str.get_dummies()], axis=1)
 
