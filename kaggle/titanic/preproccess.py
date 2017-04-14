6f1430a039ec6a5b8b58f85b716329a043c74e00
David Valpey <greengargoyle@hotmail.com>
2017-04-14 15:20:05 -0700

    use passenger id as the index

--- kaggle/titanic/preproccess.py
+++ kaggle/titanic/preproccess.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv("data/train.csv")
+df = pd.read_csv("data/train.csv", index_col=0)
 # df['Deck'] = df[~df['Cabin'].isnull()]['Cabin'].str[0]
 df['Title'] = df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
 df['FamSize'] = df['Parch'] + df['SibSp']
